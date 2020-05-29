08325c052b8cdefd31faf56ce9d31c442427295b
Igor <rogier.van.amersfoort.75@gmail.com>
2020-05-29 17:35:18 -0300

    Backend continued

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -1,24 +1,21 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
+var mongoose = require("mongoose");
 
-var campgrounds = [
-	{
-		name: "Salmon Creek",
-		image:
-			"https://www.photosforclass.com/download/pixabay-4522970?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F52e5d7414355ac14f6da8c7dda793f7f1636dfe2564c704c7c2f78dc9448c05d_1280.jpg&user=Ben_Frieden",
-	},
-	{
-		name: "Granite Hill",
-		image:
-			"https://www.photosforclass.com/download/pixabay-3893587?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F55e8dc404f5aab14f6da8c7dda793f7f1636dfe2564c704c7c2f78dc9448c05d_1280.jpg&user=FabricioMacedoPhotos",
-	},
-	{
-		name: "Mountain View",
-		image:
-			"https://www.photosforclass.com/download/pixabay-1851092?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F57e8d0424a5bae14f6da8c7dda793f7f1636dfe2564c704c7c2f78dc9448c05d_1280.jpg&user=Pexels",
-	},
-];
+mongoose.connect("mongodb://localhost:27017/yelp_camp", {
+	useNewUrlParser: true,
+	useCreateIndex: true,
+	useFindAndModify: false,
+	useUnifiedTopology: true,
+});
+
+var campgroundSchema = new mongoose.Schema({
+	name: String,
+	image: String,
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
@@ -28,7 +25,13 @@ app.get("/", function (req, res) {
 });
 
 app.get("/campgrounds", function (req, res) {
-	res.render("campgrounds", { campgrounds: campgrounds });
+	Campground.find({}, function (err, campgrounds) {
+		if (err) {
+			console.log(err);
+		} else {
+			res.render("campgrounds", { campgrounds: campgrounds });
+		}
+	});
 });
 
 app.get("/campgrounds/new", function (req, res) {
@@ -38,7 +41,13 @@ app.post("/campgrounds", function (req, res) {
 	var name = req.body.name;
 	var image = req.body.image;
 	var newCampground = { name: name, image: image };
-	campgrounds.push(newCampground);
+	Campground.create(newCampground, function (err, newOne) {
+		if (err) {
+			console.log(err);
+		} else {
+			console.log(newOne);
+		}
+	});
 	res.redirect("/campgrounds");
 });
 
